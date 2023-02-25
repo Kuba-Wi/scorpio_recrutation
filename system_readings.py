62fe0653d79e008df34df322d8156656a7a67a51
kuba <winczukjakub@gmail.com>
2023-02-25 19:46:00 +0100

    writing data to file in json format

--- system_readings.py
+++ system_readings.py
@@ -1,3 +1,4 @@
+import json
 import os
 import psutil
 import shutil
@@ -6,39 +7,36 @@ import time
 
 UPDATE_TIME = 5
 
-def write_cpu_usage(file):
+def write_cpu_usage(data_dict):
     with os.popen("mpstat -P ALL 1 1") as process:
         HEADERS_COUNT = 4
         for i in range(HEADERS_COUNT):
             process.readline()
-        file.write("CPU CORES USAGE %\n")
+        data_dict["CPU"] = []
         CPU_COUNT = psutil.cpu_count()
         for i in range(CPU_COUNT):
-            file.write(process.readline().split()[2].replace(",", "."))
-            file.write("\n")
+            data_dict["CPU"].append(process.readline().split()[2].replace(",", "."))
 
-def write_cpu_temp(file):
+def write_cpu_temp(data_dict):
     with os.popen("acpi -t") as process:
-        file.write("Temperature from all sensors\n")
+        data_dict["temp"] = []
         for line in process:
-            file.write(line.split()[3])
-            file.write("\n")
+            data_dict["temp"].append(line.split()[3])
 
-def write_RAM_usage(file):
-    file.write("RAM usage (total, used, available)\n")
-    file.write("{:.2f}\n".format(psutil.virtual_memory()[0]/(10**9)))
-    file.write("{:.2f}\n".format(psutil.virtual_memory()[3]/(10**9)))
-    file.write("{:.2f}\n".format(psutil.virtual_memory()[1]/(10**9)))
+def write_RAM_usage(data_dict):
+    data_dict["RAM"] = []
+    data_dict["RAM"].append("{:.2f}".format(psutil.virtual_memory()[0]/(10**9))) #total
+    data_dict["RAM"].append("{:.2f}".format(psutil.virtual_memory()[3]/(10**9))) #used
+    data_dict["RAM"].append("{:.2f}".format(psutil.virtual_memory()[1]/(10**9))) #available
 
-def write_disk_usage(file):
+def write_disk_usage(data_dict):
     t, u, free = shutil.disk_usage("/")
-    file.write("Free disk space\n")
-    file.write("{:.2f}\n".format(free / (10**9)))
+    data_dict["disk"] = "{:.2f}".format(free / (10**9))
 
 def get_net_iface_counters():
     return psutil.net_io_counters(pernic=True)
 
-def write_network_ifaces_info(file, io_old):
+def write_network_ifaces_info(data_dict, io_old):
     ifaces = {}
     for ifname, data in psutil.net_if_stats().items():
         ifaces[ifname] = ["UP" if data.isup else "DOWN"]
@@ -53,19 +51,16 @@ def write_network_ifaces_info(file, io_old):
         ifaces[ifname].append(recv_speed)
         ifaces[ifname].append(send_speed)
 
-    file.write("Network interfaces (status, address, receive speed, send speed [b/s])\n")
-    for ifname, data in ifaces.items():
-        file.write(ifname)
-        file.write("\n")
-        file.write(" ".join(str(d) for d in data))
-        file.write("\n")
+    data_dict["network"] = ifaces
 
 while True:
     io_old = get_net_iface_counters()
     time.sleep(UPDATE_TIME)
-    with open(os.path.expanduser("~") + "/system_data_readings.txt", "w") as file:
-        write_cpu_usage(file)
-        write_cpu_temp(file)
-        write_RAM_usage(file)
-        write_disk_usage(file)
-        write_network_ifaces_info(file, io_old)
+    data_dict = {}
+    write_cpu_usage(data_dict)
+    write_cpu_temp(data_dict)
+    write_RAM_usage(data_dict)
+    write_disk_usage(data_dict)
+    write_network_ifaces_info(data_dict, io_old)
+    with open("system_data_readings.txt", "w") as file:
+        file.write(json.dumps(data_dict))
