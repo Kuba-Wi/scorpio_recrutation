b077d3c23dffdedddc9fb43bd66318b72a69c594
kuba <winczukjakub@gmail.com>
2023-02-26 14:42:58 +0100

    small change

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require("express")
 const fs = require("fs");
-const os = require('os')
+const os = require("os")
 
 const app = express()
 const port = 5000
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   fs.readFile(os.homedir() + "/system_data_readings.txt", (err, data) => {
     if (err) {
       res.send({"result": "error"})
