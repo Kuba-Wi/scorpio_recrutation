a89d06ff104210eb9cc84589a2cf9748c3e3e490
kuba <winczukjakub@gmail.com>
2023-02-26 17:12:07 +0100

    added printing data in terminal

--- system_readings.py
+++ system_readings.py
@@ -14,24 +14,38 @@ def read_cpu_usage(data_dict):
             process.readline()
         data_dict["CPU"] = []
         CPU_COUNT = psutil.cpu_count()
-        for _ in range(CPU_COUNT):
-            data_dict["CPU"].append(process.readline().split()[2].replace(",", "."))
+        print("CPU core usage %")
+        for i in range(CPU_COUNT):
+            cpu_usage = process.readline().split()[2].replace(",", ".")
+            data_dict["CPU"].append(cpu_usage)
+            print(f"CPU{i+1} {cpu_usage}")
 
 def read_cpu_temp(data_dict):
     with os.popen("acpi -t") as process:
         data_dict["temp"] = []
+        print("Sensors temperature " + u"\u2103")
         for line in process:
             data_dict["temp"].append(line.split()[3])
+            print(line.split()[3])
 
 def read_RAM_usage(data_dict):
     data_dict["RAM"] = {}
-    data_dict["RAM"]["total"] = "{:.2f}".format(psutil.virtual_memory().total/(10**9))
-    data_dict["RAM"]["used"] = "{:.2f}".format(psutil.virtual_memory().used/(10**9))
-    data_dict["RAM"]["available"] = "{:.2f}".format(psutil.virtual_memory().available/(10**9))
+    total = "{:.2f}".format(psutil.virtual_memory().total/(10**9))
+    data_dict["RAM"]["total"] = total
+    used = "{:.2f}".format(psutil.virtual_memory().used/(10**9))
+    data_dict["RAM"]["used"] = used
+    available = "{:.2f}".format(psutil.virtual_memory().available/(10**9))
+    data_dict["RAM"]["available"] = available
+    print("RAM usage [GB]")
+    print(f"total: {total}")
+    print(f"used: {used}")
+    print(f"available: {available}")
 
 def read_disk_usage(data_dict):
     t, u, free = shutil.disk_usage("/")
-    data_dict["disk"] = "{:.2f}".format(free / (10**9))
+    free_formated = "{:.2f}".format(free / (10**9))
+    data_dict["disk"] = free_formated
+    print(f"Free disk space [GB]: {free_formated}")
 
 def get_net_iface_counters():
     return psutil.net_io_counters(pernic=True)
@@ -44,12 +58,18 @@ def read_network_ifaces_info(data_dict, io_old):
     for ifname, data in psutil.net_if_addrs().items():
         ifaces[ifname]["ip"] = data[0].address
 
+    print("Network interfaces")
     io_new = get_net_iface_counters()
     for ifname, data in io_old.items():
         recv_speed = (io_new[ifname].bytes_recv - data.bytes_recv) * 8 // UPDATE_TIME
         send_speed = (io_new[ifname].bytes_sent - data.bytes_sent) * 8 // UPDATE_TIME
         ifaces[ifname]["recv_speed"] = recv_speed
         ifaces[ifname]["send_speed"] = send_speed
+        print(ifname)
+        print(f"status: {ifaces[ifname]['status']}")
+        print(f"ip: {ifaces[ifname]['ip']}")
+        print(f"receive speed [b/s]: {recv_speed}")
+        print(f"send speed [b/s]: {send_speed}")
 
     data_dict["network"] = ifaces
 
